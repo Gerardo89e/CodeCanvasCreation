268282eacd9734be396ee37d4ec9e75f398b361e
Gerardo89e <mgerardo761@gmail.com>
2024-02-09 15:01:32 -0500

    add the router page for the navbar to go to a new page, only issue it takes time to load the whole code so if you see it not doing what its suppose to do restart the server or open a new link in a new tab and that should work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { routes } from './app.routes'; // Import your routes array
+import { RouterModule} from '@angular/router';
 import { NgbCarouselConfig, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { routes } from './app.routes';
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import { CarouselComponent } from "../carousel/carousel.component";
+import { ServiceSectionComponent } from "../service-section/service-section.component";
+import { GetStartedComponent } from "../get-started/get-started.component";
+import { MeetOurTeamComponent } from "../../meet-our-team/meet-our-team.component";
+import { GetInTouchComponent } from "../../get-in-touch/get-in-touch.component";
+import { NavbarComponent } from "../navbar/navbar.component";
+import { FooterComponent } from "../../footer/footer.component";
 
 
 @Component({
@@ -7,7 +13,7 @@ import { CarouselComponent } from "../carousel/carousel.component";
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [CarouselComponent]
+    imports: [CarouselComponent, ServiceSectionComponent, GetStartedComponent, MeetOurTeamComponent, GetInTouchComponent, NavbarComponent, FooterComponent]
 })
 export class HomeComponent {
 }
